26627a81fee039204b65f12a4b2ab90670913e06
AdamPetersPortfolio <76824403+AdamPetersPortfolio@users.noreply.github.com>
2021-01-01 15:28:06 -0500

    Create PitchCounterApp.js

---
+++ PitchCounterApp.js
@@ -0,0 +1,63 @@
+/**
+ * Summary. Contains the implementation to create a instrument dependent
+ *          pitch counter. Exports a PitchCounterApp object for
+ *          a react file to use.
+ *
+ * @file   This files defines the PitchCounterApp class.
+ * @author Adam Peters.
+ */
+
+class PitchCounterApp {
+
+    constructor(instr) {
+        this.instrument = new InstrumentListener(instr);
+        this.get_pitch_count = function () {
+            return this.instrument.get_pitch_count();
+        };
+    }
+
+    /**
+     * Change the Instrument to which the app is listening
+     *
+     * @param {instr} the String name of the instrument
+     */
+    change_instrument(instr) {
+        this.instrument.changeInstrument(instr)
+    }
+
+    /**
+     * Start the EventListener. Cannot start on opening of WebPage due
+     * to AudioContext limitations.
+     */
+    start() {
+        this.instrument.startListener();
+    }
+
+    /**
+     * Completely stop the app from listening.
+     */
+    stop() {
+        this.instrument.stopListener();
+    }
+
+    /**
+     * Pause the app from listening.
+     */
+     changeState() {
+        return this.instrument.changeListenerState();
+    }
+}
+
+app = new PitchCounterApp("piano");
+
+function Begin() {
+    app.start();
+}
+
+function Stop() {
+    app.stop();
+}
+
+function ChangeState() {
+    app.changeState();
+}
